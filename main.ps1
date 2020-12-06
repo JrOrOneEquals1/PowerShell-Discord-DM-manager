a92e2aff76d6678aa6df1eb54aefb361e36b5b16
Carrie Roberts <clr2of8@gmail.com>
2020-12-06 00:03:05 -0700

    get-botmessage

--- main.ps1
+++ main.ps1
@@ -10,10 +10,6 @@ if (-not (Test-Path $sentFile) ) { $null = New-Item -ItemType File -Path $sentFi
 $notSentFile = "$PSScriptRoot\WorkingDirectory\notSentFile.txt"
 if (-not (Test-Path $notSentFile) ) { $null = New-Item -ItemType File -Path $notSentFile -Force }
 
-# $fileName = Read-Host -Prompt "IP File Path"
-# $ipList = Get-Content -Path $fileName
-# $ips = $ipList.length - 1
-
 if ($null -eq $Driver) {
     $arguments = @()
     if ($maximized) { $arguments += 'start-maximized' }
@@ -80,31 +76,40 @@ while ($true) {
 
     $staticNotSent = $notSent.Clone()
     foreach ($id in $staticNotSent.Keys) {
-        # only continue if its been over 2 minutes since this users messages were last read
+        # only continue if its been over $longSleep seconds since this users messages were last read
         if ( ((Get-Date) - (Get-Date $notSent[$id])).TotalSeconds -lt $longSleep ) { Write-Host -Fore Yellow "Skipping $id"; continue }
         Enter-SeUrl "https://discord.com/channels/@me/$id" -Driver $Driver
-        $userName = (Find-SeElement -Driver $Driver -classname "username-1A8OIy")[0].GetAttribute("innerText")
+        $userName = (Find-SeElement -Driver $Driver -classname "title-29uC1r").GetAttribute("innerText")
         Write-Host -Fore Green "Checking $userName messages $id"
         $messagesList = Find-SeElement -Driver $Driver -classname "contents-2mQqc9" # These are all the messages in the chat
         $notSent[$id] = Get-Date -format "yyyy/MM/dd hh:mm:ss tt"
 
         $results = getNewMessages $messagesList
-        if($results | where-object {$_.text -match "(\n|^)$keyWord`$"}){
-                # Send the message
-                $chatboxes = Find-SeElement -Driver $Driver -classname "slateTextArea-1Mkdgw" # The messaging input box
-                $IP = "1.2.3.4"
-                Write-Host -Fore Cyan "Sending IP $IP to $user"
+        if ($results | where-object { $_.text -match "(\n|^)$keyWord`$" }) {
+            # Send the message
+            $chatboxes = Find-SeElement -Driver $Driver -classname "slateTextArea-1Mkdgw" # The messaging input box
+            $message = Get-BotMessage $id $userName
+            if ($message) {
                 Add-Content -Path $sentFile -Value $id # Send $id to file so the script knows to ignore new messages from that user
-                Send-SeKeys -Element $chatboxes[-1] -Keys "$IP`n" # Send the IP
+                Write-Host -Fore Cyan "Sending message to $userName"
+                foreach ($line in $message.split("`n")) {
+                    Send-SeKeys -Element $chatboxes[-1] -Keys "$line" 
+                    Send-SeKeys -Element $chatboxes[-1] -Keys "{{shift}}{{ENTER}}"
+                }
+                Send-SeKeys -Element $chatboxes[-1] -Keys "{{ENTER}}"
+                Start-Sleep 1
                 #remove this id from notsent
                 $notSent.Remove($id)
-        }
+            }
+            else { Write-Host -ForegroundColor Red "No message Returned, no message sent to $id $userName" }
+        }        
         foreach ($result in $results) {
             if (-not ($result.text -match "(\n|^)$keyWord`$")) {
-                # Mark messages as unread, click the message so the elipsis button shows up
+                # Mark message as unread, click the message so the elipsis button shows up
                 $result | Invoke-SeClick
                 (Find-SeElement -Driver $Driver -classname "button-1ZiXG9")[2] | Invoke-SeClick -Driver $Driver -JavaScriptClick # The three dots that show up on hover
-                (Find-SeElement -Driver $Driver -classname "label-22pbtT")[2]  | Invoke-SeClick -Driver $Driver -JavaScriptClick # The 'Mark Unread' button in the three dots menu    
+                (Find-SeElement -Driver $Driver -classname "label-22pbtT")[2]  | Invoke-SeClick -Driver $Driver -JavaScriptClick # The 'Mark Unread' button in the three dots menu
+                break   
             }
         }
         Enter-SeUrl "https://discord.com/channels/@me" -Driver $Driver
